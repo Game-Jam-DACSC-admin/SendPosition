c15b4733805cb050985d224b72e74dabab2a3c7a
吳孟紘 <wueric0622@gmail.com>
2017-08-05 15:15:36 +0800

    StartPoint

---
+++ Assets/CharacterPositionSender.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+using System;
+
+public class CharacterPositionSender : MonoBehaviour {
+
+    Sender Sender;
+
+    // Use this for initialization
+    void Start () {
+        Sender = GameObject.FindObjectOfType<Sender>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        SendPosition();
+
+    }
+
+    private void SendPosition()
+    {
+
+        //Sender.Data["PlayerPosition"] = transform.position.ToString();
+
+    }
+}
---
+++ Assets/Sender.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+public class Sender : MonoBehaviour {
+
+	private SocketIOComponent socket;
+    public Dictionary<string, string> Data = new Dictionary<string, string>();
+
+    // Use this for initialization
+    void Start () {
+
+        Data.Add("PlayerPosition", Vector3.zero.ToString());
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        StartCoroutine(Sending());
+    }
+
+	IEnumerator Sending()
+	{
+		yield return new WaitForSeconds(1f);
+        socket.Emit("beep",new JSONObject(Data["PlayerPosition"]));
+    }
+}
